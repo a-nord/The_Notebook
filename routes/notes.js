9fc4579e89bfaec7b246ab8f9c0e77cbaaeeac65
Ashley Nord <anord@hntb.com>
2024-01-30 18:43:51 -0500

    added the delete function but does not work

--- routes/notes.js
+++ routes/notes.js
@@ -1,11 +1,11 @@
 const notesRouter = require("express").Router();
-const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
+const { readFromFile, readAndAppend, writeToFile } = require("../helpers/fsUtils");
 const uuid = require('../helpers/uuid');
 
 //GET Route for all notes
 notesRouter.get("/", (req, res) => {
 	readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-});
+});  
 
 //POST Route for new notes
 notesRouter.post("/", (req, res) => {
@@ -25,6 +25,23 @@ notesRouter.post("/", (req, res) => {
   } else {
     res.error('Error in adding note');
   }
+});
+
+// DELETE Route for a specific note
+notesRouter.delete('/:notes_id', (req, res) => {
+  const notesId = req.params.notes_id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all tips except the one with the ID provided in the URL
+      const result = json.filter((note) => note.notes_id != notesId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/db.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${notesId} has been deleted 🗑️`);
+    });
 });
 
 
