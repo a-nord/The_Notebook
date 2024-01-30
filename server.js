ce3fc6385e4a762249b3b1bebe5e71d8387fc2f7
Ashley Nord <anord@hntb.com>
2024-01-30 17:08:26 -0500

    added the notes to the server.js file

--- server.js
+++ server.js
@@ -1,23 +1,36 @@
+// Import Express.js
 const express = require("express");
+
+// Import built-in Node.js package 'path' to resolve path of files that are located on the server
 const path = require('path');
+
+// Specify on which port the Express.js server will run
 const api = require('./routes/index.js');
+
+// Initialize an instance of Express.js
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
+// Static middleware pointing to the public folder
 app.use(express.static('public'));
 
+// GET Route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
+// GET Route for homepage
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
+// listen() method is responsible for listening for incoming connections on the specified port 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
